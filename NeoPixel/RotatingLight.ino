37dbd5760bbb7fa55c986fd07bf31bdfd2e5e593
Krishna@TECRA-Z50-A <krishna444@gmail.com>
2022-11-12 14:34:45 +0100

    Files restructured, and rotating light effect added.

---
+++ NeoPixel/RotatingLight.ino
@@ -0,0 +1,46 @@
+#include <Adafruit_NeoPixel.h>
+#include "TinkerKit.h"
+#include <math.h>
+#define PIN 7
+#define NUMPIXELS 20
+int shift=0;
+int delayMilliseconds = 50;
+int max_delay = 100, min_delay = 10;
+int selectedPixel=0;
+//int colorIndex=0;
+int rainbow[7][3]={{148, 0, 211},{75, 0, 130},{0, 0, 255},{0, 255, 0},{255, 255, 0},{255, 127, 0},{255, 0 , 0}};;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+TKLightSensor lightSensor(I0);
+
+void setup()
+{       
+    Serial.begin(115200);
+    strip.begin();
+}
+
+void loop()
+{
+    float light = lightSensor.read();
+    if (light > 30)
+    {
+        strip.clear();
+        strip.show();
+        delay(2000);
+        return;
+    }
+    for (int i = 0; i < NUMPIXELS; i++)
+    {
+        strip.setPixelColor(i, 0,0,0);
+    }   
+    for(int index=0;index<7;index++){
+        //if(selectedPixel+index<NUMPIXELS)
+        strip.setPixelColor((selectedPixel+index)%NUMPIXELS,rainbow[index][0],rainbow[index][1],rainbow[index][2]);
+    }    
+    selectedPixel++;
+    if(selectedPixel>=NUMPIXELS){
+        selectedPixel=0;
+    }
+    strip.show();
+    delay(random(min_delay, max_delay));
+}
