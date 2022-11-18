919bfd2d78870ebbebf7567779886732db0cf34b
krishna@raspi <krishna444@gmail.com>
2022-11-18 22:33:22 +0000

    Minor changes.

--- NeoPixel/NeoPixel.ino
+++ NeoPixel/NeoPixel.ino
@@ -1,12 +1,12 @@
 #include "src/RotatingColor/RotatingColor.h"
 //#include "src/RotatingRainbow/RotatingRainbow.h"
-//#include "src/SnowFall/SnowFall.cpp"
+#include "src/SnowFall/SnowFall.cpp"
 #include "src/Sequencer/Sequencer.cpp"
 #include "src/Gradient/Gradient.cpp"
 
  RotatingColor *rotatingColor;
 //RotatingRainbow *rotatingRainbow;
-// SnowFall *snowFall;
+SnowFall *snowFall;
 Sequencer *sequencer;
 Gradient *gradient;
 
@@ -14,16 +14,16 @@ void setup()
 {
    rotatingColor = new RotatingColor(20,114);
    //rotatingRainbow = new RotatingRainbow(800, 2000);
-   // snowFall = new SnowFall();
+   snowFall = new SnowFall();
    sequencer=new Sequencer();
    gradient=new Gradient();
 }
 
 void loop()
 {
-   rotatingColor->process(true);
+   //rotatingColor->process(true);
    // rotatingRainbow->process();
-   // snowFall->process();
-   //sequencer->process();
+   snowFall->process();
+  // sequencer->process();
    //gradient->process();
 }
