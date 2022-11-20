31a4f6dc84368f170ff7613bda4e4d71d94001b8
krishna@raspi <krishna444@gmail.com>
2022-11-20 18:16:38 +0000

    Leds extended.

--- Misc/src/Blinking/Leds.cpp
+++ Misc/src/Blinking/Leds.cpp
@@ -11,9 +11,17 @@ class Leds
         Leds()
         {
             this->ledLarge = new TKLed(O4);
+            this->ledRed=new TKLed(O5);
+            this->ledGreen=new TKLed(O2);
+            this->ledLarge->brightness(255);
+            this->ledRed->brightness(255);
+            this->ledGreen->brightness(255);
+            
         }
         void process()
-        {
-            this->ledLarge->blink(100,200);
+        {            
+            this->ledLarge->blink(100);
+            this->ledRed->blink(200);
+            this->ledGreen->blink(300);            
         }
 };
